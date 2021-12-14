4e2401eb5407a1eec17bbd3e6270473e1a5743a7
Emanuel120021 <emanuel.lazaro.52@gmail.com>
2021-12-14 14:07:00 -0300

    commit inicial

---
+++ src/test/java/com/emanuel/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emanuel.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
